f4b3fbc3da0f73f9bf2e1b4c730cad7f51f6d533
Thanh1999 <t.max347056@gmail.com>
2021-07-16 13:51:43 +0700

    add navigate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
 
+const HatsPage = (props) => {
+  console.log(props);
+  return (<div>
+    <h1>Hats Page</h1>
+
+  </div>);
+}
+
 function App() {
   return (
-    <HomePage/>
+    <div>
+      <Switch>
+        <Route exact component={HomePage} path='/' />
+        <Route component={HatsPage} path='/hats' />
+      </Switch>
+    </div>
   );
 }
 
