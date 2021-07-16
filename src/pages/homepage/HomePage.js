ef1d451679935a436e710f462e8bbdf15fec3760
Thanh1999 <t.max347056@gmail.com>
2021-07-16 11:45:10 +0700

    seperate components

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,9 @@
+import Directory from '../../components/directory/directory'
+import './HomePage.scss'
+
+const HomePage = () =>
+    <div className='homepage'>
+        <Directory />
+    </div>
+
+export default HomePage
