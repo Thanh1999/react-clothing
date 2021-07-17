ff2423ca7b0d9aa27b570fc223d0c0e953da199f
Thanh1999 <t.max347056@gmail.com>
2021-07-17 23:50:31 +0700

    finish sign-up

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import './sign-in.scss'
 import FormInput from '../form-input/form-input'
 import CustomButton from '../button/button';
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 class SignIn extends Component {
     constructor() {
@@ -13,12 +13,22 @@ class SignIn extends Component {
         }
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
-        this.setState({
-            email: '',
-            password: ''
-        })
+
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+
+            this.setState({
+                email: '',
+                password: ''
+            })
+
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     handleChange = (event) => {
