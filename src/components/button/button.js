f54a37e3f96e4ba7b9f35d8964e3931c3319457d
Thanh1999 <t.max347056@gmail.com>
2021-10-10 23:01:43 +0700

    add sql data script

--- src/components/button/button.js
+++
@@ -1,8 +0,0 @@
-import { CustomButtonContainer } from './button.styles'
-
-const CustomButton = ({ children, ...otherProps }) =>
-    <CustomButtonContainer {...otherProps}>
-        {children}
-    </CustomButtonContainer>
-
-export default CustomButton
